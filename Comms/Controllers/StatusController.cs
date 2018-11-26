aa0458926d47b7ccfda90f1e693acacb8bebed44
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-26 19:32:51 +0000

    Add Status controller

---
+++ Comms/Controllers/StatusController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Comms.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
