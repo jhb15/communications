c51c1b768b1bb79ac20e5c2b91f546fd710a69b2
Adam Lancaster <arglanc@users.noreply.github.com>
2018-11-27 14:47:21 +0000

    Create IEmailSender.cs

---
+++ Comms/Services/IEmailSender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comms.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string to, string subject, string htmlContent);
+    }
+
+    public class EmailException : Exception
+    {
+        public EmailException(string message) : base(message)
+        {
+        }
+    }
+}
